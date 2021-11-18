0564e59a0cc0cf790a55b0c403bce73d5c36ec67
Andie Fonderson <Andrea.Fonderson@IRIS.CO.UK>
2021-11-18 14:22:12 +0000

    Added functionality to select whether to show amounts owed or account transactions.

--- Program.cs
+++ Program.cs
@@ -7,62 +7,115 @@ namespace SupportBank
     internal class Program
     {
         static string path = @"C:\Users\Andrea.Fonderson\OneDrive - IRIS Software Group\Corndel Bootcamp\C Sharp Bootcamp\2 - SupportBank\Data Files\Transactions2014.csv";
-        static string textFile = File.ReadAllText(path);
+        static string[] fileByLines = File.ReadAllLines(path);
+
+        static Dictionary<string, double> accountMoneyOwedOnly = new Dictionary<string, double>();
+        static Dictionary<string, string> accountFullDetails = new Dictionary<string, string>();
 
         static void Main(string[] args)
         {
-            ReadFile();
+            ProgramStartUp();
         }
 
-        static void ReadFile()
-        {
-            string[] fileByLines = File.ReadAllLines(path);
-            Dictionary<string, double> account = new Dictionary<string, double>();
-
+        static void ProgramStartUp()
+        {       
             for (int i = 1; i < fileByLines.Length; i++)
             {
                 string[] cell = fileByLines[i].Split(",");
+                string date = cell[0];
                 string fromName = cell[1];
                 string toName = cell[2];
                 string narrative = cell[3];
-                double amount = double.Parse(cell[4]);                
+                double amount = double.Parse(cell[4]);
 
-                if (!account.ContainsKey(fromName))
-                {
-                    account.Add(fromName,amount);
-                }
-                else
-                {
-                    account[fromName] += amount;
-                }
+                string accountLineText = $"{date}: [Incoming] Paid {amount:C} by {toName} for {narrative}";
+                string accountPaidOtherLineText = $"{date}: [Outgoing] Paid {amount:C} to {fromName} for {narrative}";
 
-                if (!account.ContainsKey(toName))
-                {
-                    account.Add(toName, -amount);
-                }
-                else
-                {
-                    account[toName] -= amount;
-                }
-
-                /*
-                Account fullAccountLineDetails = new Account()
-                {
-                    date = cell[0],
-                    fromName = cell[1],
-                    toName = cell[2],
-                    narrative = cell[3],
-                    amount = double.Parse(cell[4])
-                };
-
-                if (!account.ContainsKey(accountLine.fromName))
-                {
-                    account.Add(cell[1], accountLine);
-                }
-                */
+                TotalOwedMoney(fromName, amount);
+                TotalOwedMoney(toName, -amount);
+                ListAccountDetails(fromName, accountLineText);
+                ListAccountDetails(toName, accountPaidOtherLineText);
             }
 
-            ListAllAccounts(account);
+            MainMenu();         
+        }
+
+        static void MainMenu()
+        {
+            Console.WriteLine("Please select the number of the task you want to carry out." +
+                "\n1) List All Account Balances" +
+                "\n2) List Account Transactions");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    ListAllAccounts(accountMoneyOwedOnly);
+                    break;
+                case "2":
+                    ListFullAccountTransactions();
+                    break;
+                default:
+                    Console.WriteLine("That was not a valid choice. Please try again.");
+                    MainMenu();
+                    break;
+            }
+
+            ReturnToMainMenu();
+        }
+
+        static void ListFullAccountTransactions()
+        {
+            string nameList = "";
+
+            if (accountFullDetails.Count != 0)
+            {
+                Console.WriteLine("\nPlease select whose account you want to access from the following:");
+                foreach (var name in accountFullDetails)
+                {
+                    nameList += $"{name.Key} | ";
+                }
+
+                Console.WriteLine(nameList);
+
+                try
+                {
+                    ListAccountName(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\nYou didn't input a valid name. Please try again.");
+                    ListAccountName(Console.ReadLine());
+                }
+            }
+            else { Console.WriteLine("\nNo names were found within this document to enable an account search."); }
+
+            Console.WriteLine("\nWould you like to search for another account? If so, type 'Y'.");
+            if(Console.ReadLine().ToUpper() == "Y")
+            {
+                ListFullAccountTransactions();
+            }
+
+        }
+
+        private static void ListAccountDetails(string name, string lineText)
+        {
+            if (!accountFullDetails.ContainsKey(name))
+            {
+                accountFullDetails.Add(name, $"{name} Statement \n{lineText}");
+            }
+            else { accountFullDetails[name] += $"\n{lineText}"; }
+        }
+
+        private static void TotalOwedMoney(string name, double amount)
+        {
+            if (!accountMoneyOwedOnly.ContainsKey(name))
+            {
+                accountMoneyOwedOnly.Add(name, amount);
+            }
+            else
+            {
+                accountMoneyOwedOnly[name] += amount;
+            }
         }
 
         static void ListAllAccounts(Dictionary<string, double> accountBook)
@@ -71,20 +124,28 @@ namespace SupportBank
             {
                 if(line.Value < 0)
                 {
-                    Console.WriteLine($"{line.Key} is owed {-line.Value:£0.00}");
+                    Console.WriteLine($"{line.Key} is owed {-line.Value:C}.");
                 }
                 else
                 {
-                    Console.WriteLine($"{line.Key} owes {line.Value:£0.00}");
+                    Console.WriteLine($"{line.Key} owes {line.Value:C}!!");
                 }                
             }
         }
 
-        static string ListAccountOutstanding(string name, double amount)
+        static void ListAccountName(string name)
         {
-            return $"{name} owes {amount:£0.00}";
+            Console.WriteLine(accountFullDetails[name]);
+        }
+
+        static void ReturnToMainMenu()
+        {
+            Console.WriteLine("\nWould you like to do something else? " +
+                "\nIf so, type 'Y'. Otherwise type any key to close.");
+            if (Console.ReadLine().ToUpper() == "Y")
+            {
+                MainMenu();
+            }
         }
     }
-
-
 }
