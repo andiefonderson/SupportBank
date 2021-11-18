259deb55834ad595dc193990e23db4fc8e09ecff
Andie Fonderson <Andrea.Fonderson@IRIS.CO.UK>
2021-11-18 09:08:39 +0000

    Initial commit

---
+++ SupportBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SupportBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
